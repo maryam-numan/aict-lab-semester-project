783672374aec8ae4d3b01dc4b7a6d95316dafa11
Iqra-Jahangir <iqraiqrajahangir1509@gmail.com>
2025-01-05 22:57:37 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('open');
+  }
+  
+  
+  document.addEventListener('click', function(event) {
+    const sidebar = document.getElementById('sidebar');
+    const sidebarBtn = document.querySelector('.sidebar-btn');
+    
+    // Check if the click is outside the sidebar and sidebar button
+    if (!sidebar.contains(event.target) && !sidebarBtn.contains(event.target)) {
+      sidebar.style.display = 'none'; // Close sidebar
+    }
+  });
