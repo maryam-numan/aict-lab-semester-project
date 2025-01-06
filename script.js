a8e3696ba86141ad7db9a6f5ae90f29870ebb5e5
Maryam Numan <maryam.numan@gmail.com>
2025-01-06 10:02:30 +0500

    Update script.js
    Updating the javascript file for all webpages

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 function toggleSidebar() {
-    const sidebar = document.getElementById('sidebar');
-    sidebar.classList.toggle('open');
+  const sidebar = document.getElementById('sidebar');
+  sidebar.classList.toggle('open');
+}
+
+document.addEventListener('click', function(event) {
+  const sidebar = document.getElementById('sidebar');
+  const sidebarBtn = document.querySelector('.sidebar-btn');
+  
+  // Check if the click is outside the sidebar and sidebar button
+  if (!sidebar.contains(event.target) && !sidebarBtn.contains(event.target)) {
+    sidebar.classList.remove('open'); // Close sidebar by removing the 'open' class
   }
-  
-  
-  document.addEventListener('click', function(event) {
-    const sidebar = document.getElementById('sidebar');
-    const sidebarBtn = document.querySelector('.sidebar-btn');
-    
-    // Check if the click is outside the sidebar and sidebar button
-    if (!sidebar.contains(event.target) && !sidebarBtn.contains(event.target)) {
-      sidebar.style.display = 'none'; // Close sidebar
-    }
-  });
+});
